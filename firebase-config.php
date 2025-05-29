724078d04480af6b54c25a5412005cdff551d762
Agustín Aisen <agusaisen@gmail.com>
2025-05-29 10:36:56 -0300

    Update firebase-config.php

--- firebase-config.php
+++ firebase-config.php
@@ -1,13 +1,13 @@
 <?php
-// Configuración de Firebase (oculta en el servidor)
+// Configuración de Firebase 
 $config = [
-    "apiKey" => "AIzaSyCxnJ8Ii9OlXzP-kDYxDs8OB0uQP-jpUVM",
-    "authDomain" => "goil-community.firebaseapp.com",
-    "projectId" => "goil-community",
-    "storageBucket" => "goil-community.appspot.com",
-    "messagingSenderId" => "546325875679",
-    "appId" => "1:546325875679:web:ba325ca83cfef474e2d26a",
-    "measurementId" => "G-4WKN4PF3QW"
+    "apiKey" => "*******", 
+    "authDomain" => "******",
+    "projectId" => "*******",
+    "storageBucket" => "*********",
+    "messagingSenderId" => "***********",
+    "appId" => "**********",
+    "measurementId" => "********"
 ];
 
 // Convertir el array a JSON y devolverlo
