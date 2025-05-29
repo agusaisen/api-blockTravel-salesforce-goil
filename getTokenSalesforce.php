27fb256bd284c35ffe3e9cbdb8157afb5d804492
Agustín Aisen <agusaisen@gmail.com>
2025-05-29 10:46:01 -0300

    Update getTokenSalesforce.php

--- getTokenSalesforce.php
+++ getTokenSalesforce.php
@@ -1,14 +1,14 @@
 <?php
 session_start(); // Inicia la sesión
-$login = $_SESSION['login'] ?? "login";
+$login = $_SESSION['login'] ?? "login"; // Para ver si es cliente o empresa
 // Configuración
-$clientId = '3MVG9YaE55JGKFNM6EyoQ3OS14s3KcNfJ0YZbBMcmZhtCavT4zHsw0p1FGdH_94eCvYseks8GZlHJm3J7QWf7';  // Reemplaza con tu Consumer Key
-$clientSecret = '2031D499D37F22B370EE187DC5152F383DE76F5CFC14CF7CB2F48529FAB1F97B';  // Reemplaza con tu Consumer Secret
+$clientId = '********';  // Salesforce Consumer Key
+$clientSecret = '**********';  // Salesforce Consumer Secret
 
-$username = 'integraciones@block.com'; // Reemplaza con el nombre de usuario de Salesforce
-$password = 'Salesforce1!@'; // Reemplaza con la contraseña de Salesforce
-$securityToken = 'oyyzopJ8msO2B5PD4RHSxR1B'; // Agrega tu token de seguridad al final de la contraseña
-$loginUrl = 'https://login.salesforce.com/services/oauth2/token'; // Usa "https://test.salesforce.com" si es sandbox
+$username = '******'; // Usuario de Salesforce
+$password = '*********'; // Contraseña de Salesforce
+$securityToken = '********'; // Token de seguridad
+$loginUrl = 'https://login.salesforce.com/services/oauth2/token'; // Salesforce login url - Usar "https://test.salesforce.com" si es sandbox 
 
 // Datos del POST para obtener el token
 $postFields = [
@@ -36,7 +36,7 @@ if (curl_errno($ch)) {
     curl_close($ch);
     //exit;
     sleep(2);
-    header('Location: https://blocktravelagency.com/' . $login);   //redirigir al login
+    header('Location: https://blocktravelagency.com/' . $login);   // Redirigir al login
 }
 
 // Cierra cURL
@@ -46,11 +46,9 @@ curl_close($ch);
 $data = json_decode($response, true);
 
 // Verifica si la autenticación fue exitosa
-if (isset($data['access_token'])) {
-    
+if (isset($data['access_token'])) {    
     $_SESSION['access_token'] = $data['access_token']; // Almacena el token en la sesión
     $_SESSION['instance_url'] = $data['instance_url']; // Almacena la URL de instancia
-    //header('Location: getData.php'); // Redirige a la página donde usarás el token solo PHP
     header('Location: https://blocktravelagency.com/internal'); // Redirige a la página donde usarás el token HTML
     exit();
 } else {
@@ -58,7 +56,7 @@ if (isset($data['access_token'])) {
     echo "Error al obtener el token de acceso:<br>";
     print_r($data);
     sleep(2);
-    header('Location: https://blocktravelagency.com/' . $login);   //redirigir al login
+    header('Location: https://blocktravelagency.com/' . $login);   // Redirigir al login
 }
 
 ?>
