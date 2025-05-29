e1722de82ed96a3a9501eb2dbaf785646f00b5d9
Agustín Aisen <agusaisen@gmail.com>
2025-05-29 10:46:50 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$login=$_SESSION['login'] ?? "login";
+$login=$_SESSION['login'] ?? "login"; // Para ver si es cliente o empresa
 session_unset(); // Elimina todas las variables de sesión
 session_destroy(); // Destruye la sesión
-header('Location: https://blocktravelagency.com/' . $login);   //redirigir al login
+header('Location: https://blocktravelagency.com/' . $login);   // Redirigir al login
 exit();
